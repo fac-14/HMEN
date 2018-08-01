e9be66fc84902840210576c2907b0a03fce6ff5c
harryyy27 <harryyy27@gmail.com>
2018-08-01 14:33:20 +0100

    handling lint

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,25 @@
+const fs = require('fs');
+const path = require('fs');
+
+function publicPath(fileName) { path.join(__dirname, '..', 'public', fileName); }
+function returnError(error, res) {
+  console.log(error);
+  res.writeHead(500, 'Content-Type : text/html');
+  res.end("You've fucked up");
+}
+
 const handlers = {
-  indexHandler: (req, res) => req + res,
+  indexHandler: (req, res) => {
+    fs.readFile(publicPath('index.html'),
+      (error, file) => {
+        if (error) {
+          returnError(error, res);
+        } else {
+          res.writeHead(200, 'Content-Type:text/html');
+          res.end(file);
+        }
+      });
+  },
   queryHandler: (req, res) => res + req,
 };
 
