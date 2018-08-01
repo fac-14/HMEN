b1c8fa9cac2f773c96d1519e4191ce4ac7c0700d
harryyy27 <harryyy27@gmail.com>
2018-08-01 16:14:54 +0100

    pubes handled

--- src/router.js
+++ src/router.js
@@ -4,10 +4,10 @@ const router = (req, res) => {
   const reqUrl = req.url;
   if (reqUrl === '/') {
     handlers.indexHandler(req, res);
-  } else if (reqUrl.indexOf('/public/') !== -1) {
-    // handlers.pubicHandler(req, res);
+  } else if (req.url.indexOf('/public/') !== -1) {
+    handlers.pubicHandler(req, res);
   } else if (reqUrl.indexOf('/?q=') !== -1) {
-    // handlers.queryHandler(req, res);
+    handlers.queryHandler(req, res);
   }
 };
 
