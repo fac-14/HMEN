840999d3c54743a02536a24328e77d7dd316486e
misia <monika.flisowska@gmail.com>
2018-08-02 12:48:19 +0100

    fix conflict by saving

--- src/handler.js
+++ src/handler.js
@@ -15,25 +15,16 @@ function returnError(error, res) {
   res.writeHead(500, { 'Content-Type': 'text/html' });
   res.end('You\'ve fucked up');
 }
-<<<<<<< HEAD
-//---------------API REQUEST
-function apiRequest(req, url) {
-=======
 
 function apiRequest(req, res, url) {
->>>>>>> master
   request(url, (error, response, body) => {
     console.log('Error: ', error);
     // console.log('statusCode: ', response && response.statusCode);
     const parsedData = JSON.parse(body);
-<<<<<<< HEAD
     // console.log('Body: ', parsedData.response.results[0].fields.bodyText);
     console.log(parsedData.response.docs[0].multimedia[0].url);
-=======
-    console.log('Body: ', parsedData.response.results[0].fields.bodyText);
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(JSON.stringify(parsedData.response.results[0].fields.bodyText));
->>>>>>> master
   });
 }
 //---------------HANDLERS
@@ -78,16 +69,12 @@ const handlers = {
 //------------------URL CONSTRUCTOR
     const query = req.url.split('?q=')[1].split('&')[0];
     const guardianUrl = `https://content.guardianapis.com/search?q=${query}&show-fields=bodyText&api-key=${config.GUARDIAN_KEY}`;
-<<<<<<< HEAD
     const nytUrl = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=${config.NYT_KEY}`;
 
     // apiRequest(req, guardianUrl);
-    apiRequest(req,nytUrl);
+    apiRequest(req, res, nytUrl);
     // console.log(res);
-=======
-    // make nyt url
     apiRequest(req, res, guardianUrl);
->>>>>>> master
   },
 };
 
